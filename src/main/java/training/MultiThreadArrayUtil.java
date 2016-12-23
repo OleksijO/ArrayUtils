23e1abbeb61725cbb95892fa67d8413297f48558
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-12-23 11:38:39 +0200

    added atomic CAS algo

--- src/main/java/training/MultiThreadArrayUtil.java
+++ src/main/java/training/MultiThreadArrayUtil.java
@@ -3,6 +3,7 @@ package training;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * This class contains static method utils for arrays using multi-threading
@@ -55,12 +56,12 @@ public class MultiThreadArrayUtil {
         return counters;
     }
 
-    private static void startCount(List<SumCounter> counters) {
+    private static void startCount(List<? extends Thread> counters) {
         counters.forEach(Thread::start);
     }
 
-    private static void waitForAllDone(List<SumCounter> counters) {
-        for (SumCounter counter : counters) {
+    private static void waitForAllDone(List<? extends Thread> counters) {
+        for (Thread counter : counters) {
             try {
                 counter.join();
             } catch (InterruptedException e) {
@@ -94,6 +95,93 @@ public class MultiThreadArrayUtil {
         public void run() {
             for (int i = offset; i < array.length; i += interval) {
                 localSum += array[i];
+            }
+        }
+    }
+
+    /**
+     * Counts ArraySum in several threads to One atomic var
+     *
+     * @param array array to count sum of its elements
+     * @param threadsNumber Number of threads, which will be created to perform count
+     * @return The sum of array's elements.
+     */
+    public static long countSumWithSpecifiedNumberOfThreadsToAtomicVar(int[] array, int threadsNumber) {
+        AtomicLong sum=new AtomicLong(0);
+        List<SumToAtomicCounter> counters = createSumToAtomicCounters(array, threadsNumber, sum);
+        startCount(counters);
+        waitForAllDone(counters);
+        return sum.get();
+    }
+
+    private static List<SumToAtomicCounter> createSumToAtomicCounters(int[] array, int threadsNumber, AtomicLong sum) {
+        List<SumToAtomicCounter> counters = new ArrayList<>();
+        for (int i = 0; i < threadsNumber; i++) {
+            counters.add(new SumToAtomicCounter(array, i, threadsNumber,sum));
+        }
+        return counters;
+    }
+
+
+    static class SumToAtomicCounter extends Thread {
+        private int[] array;
+        int offset;
+        int interval;
+        AtomicLong sum;
+
+        public SumToAtomicCounter(int[] array, int offset, int interval, AtomicLong sum) {
+            this.array = array;
+            this.offset = offset;
+            this.interval = interval;
+            this.sum = sum;
+        }
+
+         @Override
+        public void run() {
+            for (int i = offset; i < array.length; i += interval) {
+                sum.getAndAdd(array[i]);
+            }
+        }
+    }
+
+    public static long countSumWithSpecifiedNumberOfThreadsToAtomicVarCAS(int[] array, int threadNumber) {
+        AtomicLong sum=new AtomicLong(0);
+        List<SumToAtomicCounterCAS> counters = createSumToAtomicCountersCAS(array, threadNumber, sum);
+        startCount(counters);
+        waitForAllDone(counters);
+        return sum.get();
+    }
+
+    private static List<SumToAtomicCounterCAS> createSumToAtomicCountersCAS(int[] array, int threadsNumber, AtomicLong sum) {
+        List<SumToAtomicCounterCAS> counters = new ArrayList<>();
+        for (int i = 0; i < threadsNumber; i++) {
+            counters.add(new SumToAtomicCounterCAS(array, i, threadsNumber,sum));
+        }
+        return counters;
+    }
+
+    static class SumToAtomicCounterCAS extends Thread {
+        private int[] array;
+        int offset;
+        int interval;
+        AtomicLong sum;
+
+        public SumToAtomicCounterCAS(int[] array, int offset, int interval, AtomicLong sum) {
+            this.array = array;
+            this.offset = offset;
+            this.interval = interval;
+            this.sum = sum;
+        }
+
+        @Override
+        public void run() {
+            long expected;
+            long newValue;
+            for (int i = offset; i < array.length; i += interval) {
+                do{
+                    expected = sum.get();
+                    newValue = expected+array[i];
+                } while (!sum.compareAndSet(expected,newValue));
             }
         }
     }
--- src/main/java/training/MultiThreadSumCountingRunner.java
+++ src/main/java/training/MultiThreadSumCountingRunner.java
@@ -9,42 +9,68 @@ public class MultiThreadSumCountingRunner {
         for (int i = 1; i < 16; i++) {
             performMultiThreadingCount(array, i);
         }
-        for (int i = 16 ; i <= 128; i+=16) {
-            performMultiThreadingCount(array, i);
+        for (int i = 1; i < 4; i++) {
+            performMultiThreadingCountToAtomic(array, i);
         }
+        for (int i = 4; i <= 16; i+=4) {
+            performMultiThreadingCountToAtomic(array, i);
+        }
+        for (int i = 1; i < 4; i++) {
+            performMultiThreadingCountToAtomicCAS(array, i);
+        }
+        for (int i = 4; i <= 16; i+=4) {
+            performMultiThreadingCountToAtomicCAS(array, i);
+        }
+
         countSumInCurrentThread(array);
         countSumInParallelStreams(array);
     }
+
+
     // OUTPUT:
     /*
-        Counting sum array[40000000] in current thread. 	Time = 417 ms. Sum =799999980000000
-        Counting sum array[40000000] in parallel streams. 	Time = 167 ms. Sum =799999980000000
-        Counting sum array[40000000] with 1 thread. 	Time = 103 ms. Sum =799999980000000
-        Counting sum array[40000000] with 2 thread. 	Time = 55 ms. Sum =799999980000000
-        Counting sum array[40000000] with 3 thread. 	Time = 37 ms. Sum =799999980000000
-        Counting sum array[40000000] with 4 thread. 	Time = 40 ms. Sum =799999980000000
-        Counting sum array[40000000] with 5 thread. 	Time = 35 ms. Sum =799999980000000
-        Counting sum array[40000000] with 6 thread. 	Time = 36 ms. Sum =799999980000000
-        Counting sum array[40000000] with 7 thread. 	Time = 37 ms. Sum =799999980000000
-        Counting sum array[40000000] with 8 thread. 	Time = 37 ms. Sum =799999980000000
-        Counting sum array[40000000] with 9 thread. 	Time = 58 ms. Sum =799999980000000
-        Counting sum array[40000000] with 10 thread. 	Time = 58 ms. Sum =799999980000000
-        Counting sum array[40000000] with 11 thread. 	Time = 57 ms. Sum =799999980000000
-        Counting sum array[40000000] with 12 thread. 	Time = 69 ms. Sum =799999980000000
-        Counting sum array[40000000] with 13 thread. 	Time = 51 ms. Sum =799999980000000
-        Counting sum array[40000000] with 14 thread. 	Time = 86 ms. Sum =799999980000000
-        Counting sum array[40000000] with 15 thread. 	Time = 105 ms. Sum =799999980000000
-        Counting sum array[40000000] with 16 thread. 	Time = 74 ms. Sum =799999980000000
-        Counting sum array[40000000] with 32 thread. 	Time = 201 ms. Sum =799999980000000
-        Counting sum array[40000000] with 48 thread. 	Time = 308 ms. Sum =799999980000000
-        Counting sum array[40000000] with 64 thread. 	Time = 372 ms. Sum =799999980000000
-        Counting sum array[40000000] with 80 thread. 	Time = 282 ms. Sum =799999980000000
-        Counting sum array[40000000] with 96 thread. 	Time = 154 ms. Sum =799999980000000
-        Counting sum array[40000000] with 112 thread. 	Time = 114 ms. Sum =799999980000000
-        Counting sum array[40000000] with 128 thread. 	Time = 166 ms. Sum =799999980000000
-        Counting sum array[40000000] in current thread. 	Time = 548 ms. Sum =799999980000000
-        Counting sum array[40000000] in parallel streams. 	Time = 131 ms. Sum =799999980000000
+        Counting sum array[40000000] in current thread. 	Time = 617 ms. Sum =799999980000000
+        Counting sum array[40000000] in parallel streams. 	Time = 162 ms. Sum =799999980000000
+        Counting sum array[40000000] with 1 thread. 	Time = 96 ms. Sum =799999980000000
+        Counting sum array[40000000] with 2 thread. 	Time = 45 ms. Sum =799999980000000
+        Counting sum array[40000000] with 3 thread. 	Time = 31 ms. Sum =799999980000000
+        Counting sum array[40000000] with 4 thread. 	Time = 24 ms. Sum =799999980000000
+        Counting sum array[40000000] with 5 thread. 	Time = 34 ms. Sum =799999980000000
+        Counting sum array[40000000] with 6 thread. 	Time = 29 ms. Sum =799999980000000
+        Counting sum array[40000000] with 7 thread. 	Time = 28 ms. Sum =799999980000000
+        Counting sum array[40000000] with 8 thread. 	Time = 21 ms. Sum =799999980000000
+        Counting sum array[40000000] with 9 thread. 	Time = 42 ms. Sum =799999980000000
+        Counting sum array[40000000] with 10 thread. 	Time = 37 ms. Sum =799999980000000
+        Counting sum array[40000000] with 11 thread. 	Time = 30 ms. Sum =799999980000000
+        Counting sum array[40000000] with 12 thread. 	Time = 35 ms. Sum =799999980000000
+        Counting sum array[40000000] with 13 thread. 	Time = 19 ms. Sum =799999980000000
+        Counting sum array[40000000] with 14 thread. 	Time = 63 ms. Sum =799999980000000
+        Counting sum array[40000000] with 15 thread. 	Time = 100 ms. Sum =799999980000000
+        Counting sum array[40000000] with 1 thread. to atomic 	Time = 570 ms. Sum =799999980000000
+        Counting sum array[40000000] with 2 thread. to atomic 	Time = 1671 ms. Sum =799999980000000
+        Counting sum array[40000000] with 3 thread. to atomic 	Time = 2571 ms. Sum =799999980000000
+        Counting sum array[40000000] with 4 thread. to atomic 	Time = 3119 ms. Sum =799999980000000
+        Counting sum array[40000000] with 8 thread. to atomic 	Time = 4870 ms. Sum =799999980000000
+        Counting sum array[40000000] with 12 thread. to atomic 	Time = 4719 ms. Sum =799999980000000
+        Counting sum array[40000000] with 16 thread. to atomic 	Time = 5133 ms. Sum =799999980000000
+        Counting sum array[40000000] with 1 thread. to atomic CAS 	Time = 476 ms. Sum =799999980000000
+        Counting sum array[40000000] with 2 thread. to atomic CAS 	Time = 1470 ms. Sum =799999980000000
+        Counting sum array[40000000] with 3 thread. to atomic CAS 	Time = 2600 ms. Sum =799999980000000
+        Counting sum array[40000000] with 4 thread. to atomic CAS 	Time = 2840 ms. Sum =799999980000000
+        Counting sum array[40000000] with 8 thread. to atomic CAS 	Time = 4852 ms. Sum =799999980000000
+        Counting sum array[40000000] with 12 thread. to atomic CAS 	Time = 4892 ms. Sum =799999980000000
+        Counting sum array[40000000] with 16 thread. to atomic CAS 	Time = 5018 ms. Sum =799999980000000
+        Counting sum array[40000000] in current thread. 	Time = 550 ms. Sum =799999980000000
+        Counting sum array[40000000] in parallel streams. 	Time = 148 ms. Sum =799999980000000
     */
+
+    private static int[] createIntFilledArray(int totalNumber) {
+        int[] arr = new int[totalNumber];
+        for (int i = 0; i < totalNumber; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
 
     private static void countSumInCurrentThread(int[] array) {
         long startTime = System.currentTimeMillis();
@@ -65,13 +91,21 @@ public class MultiThreadSumCountingRunner {
                 "\tTime = " + (System.currentTimeMillis() - startTime) + " ms. Sum =" + sum);
     }
 
-    private static int[] createIntFilledArray(int totalNumber) {
-        int[] arr = new int[totalNumber];
-        for (int i = 0; i < totalNumber; i++) {
-            arr[i] = i;
-        }
-        return arr;
+    private static void performMultiThreadingCountToAtomic(int[] array, int threadNumber) {
+        long startTime = System.currentTimeMillis();
+        long sum = MultiThreadArrayUtil.countSumWithSpecifiedNumberOfThreadsToAtomicVar(array, threadNumber);
+        System.out.println("Counting sum array[" + array.length + "] with " + threadNumber + " thread. to atomic " +
+                "\tTime = " + (System.currentTimeMillis() - startTime) + " ms. Sum =" + sum);
     }
+
+    private static void performMultiThreadingCountToAtomicCAS(int[] array, int threadNumber) {
+        long startTime = System.currentTimeMillis();
+        long sum = MultiThreadArrayUtil.countSumWithSpecifiedNumberOfThreadsToAtomicVarCAS(array, threadNumber);
+        System.out.println("Counting sum array[" + array.length + "] with " + threadNumber + " thread. to atomic CAS " +
+                "\tTime = " + (System.currentTimeMillis() - startTime) + " ms. Sum =" + sum);
+    }
+
+
 
 
 }
